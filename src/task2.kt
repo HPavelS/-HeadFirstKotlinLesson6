ece5006553f9da12c90ebbe6790dce988cd8f3d3
HPavel <pasharikwork@gmail.com>
2021-04-26 16:58:12 +0300

    add file task2 and solve this task

---
+++ src/task2.kt
@@ -0,0 +1,28 @@
+data class Student(val firstName: String, val lastName: String,
+                   val house: String, val year: Int = 1)
+fun main(args: Array<String>) {
+    val s1 = Student("Ron", "Weasley", "Gryffindor")
+    val s2 = Student("Draco", "Malfoy", house = "Slytherin")
+    val s3 = s1.copy(firstName = "Fred", year = 3)
+    val s4 = s3.copy(firstName = "George")
+    val array = arrayOf(s1, s2, s3, s4)
+    for ((firstName, lastName, house, year) in array) {
+        println("$firstName $lastName is in $house year $year")
+    }
+    println("----------------------------------")
+    main2()
+}
+
+data class Student2(val firstName: String, val lastName: String,
+                   val house: String, val year: Int = 1)
+fun main2() {
+    val s1 = Student2("Ron", "Weasley", "Gryffindor")
+    val s2 = Student2(lastName = "Malfoy", firstName = "Draco", year = 1, house = "Slitherin") //change
+    val s3 = s1.copy(firstName = "Fred", year = 3)  //change
+//    s3.year = 3
+    val s4 = s3.copy(firstName = "George")
+    val array = arrayOf(s1, s2, s3, s4)
+    for (s in array) {
+        println("${s.firstName} ${s.lastName} is in ${s.house} year ${s.year}")
+    }
+}
